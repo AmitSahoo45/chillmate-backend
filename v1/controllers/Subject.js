98aad6691c61458aabbd27316383ab92a97fe668
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-04-23 12:47:14 +0530

    Updated controllers' code for Subject

--- v1/controllers/Subject.js
+++ v1/controllers/Subject.js
@@ -169,12 +169,25 @@ const SharingSubject = async (req, res) => {
         if (!mongoose.Types.ObjectId.isValid(id))
             return res.status(StatusCodes.NOT_FOUND).json({ message: 'Subject not found' })
 
+        const page = Number(req.query.page) || 1;
+        const limit = 10;
+        const INDEX = (page - 1) * limit;
+        // count total number of documents for the chapter 
+        const total = await TextNotes.countDocuments({ Subject: id })
+
         const notes = await TextNotes
             .find({ Subject: id })
-            .populate('UserRef', 'name photoURL')
+            .select('header desc tags createdAt userGleID')
             .sort({ createdAt: -1 })
+            .limit(limit)
+            .skip(INDEX)
 
-        res.status(StatusCodes.OK).json({ notes, message: 'Notes fetched successfully' })
+        const subject = await Subject
+            .findById(id)
+            .select('Subname Subdesc Subtags Likes')
+            .populate('UserRef', 'name photoURL')
+
+        res.status(StatusCodes.OK).json({ subject, notes, message: 'Notes fetched successfully' })
     } catch (error) {
         res.status(StatusCodes.NOT_FOUND).json({ message: error.message })
     }
